e3b87110820821094383d95670b35887017aa904
Template Janitor <template.janitor@example.com>
2024-12-23 01:42:01 +0000

    Cleanup template and initialize repository

--- cmake/LibFuzzer.cmake
+++ cmake/LibFuzzer.cmake
@@ -1,4 +1,4 @@
-function(myproject_check_libfuzzer_support var_name)
+function(cppweekly_rpg_check_libfuzzer_support var_name)
   set(LibFuzzerTestSource
       "
 #include <cstdint>
--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,5 +1,5 @@
 function(
-  myproject_enable_sanitizers
+  cppweekly_rpg_enable_sanitizers
   project_name
   ENABLE_SANITIZER_ADDRESS
   ENABLE_SANITIZER_LEAK
